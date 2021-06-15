685ce5b405d7ee7058175e8d88b808bb16ecea8d
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-06-15 22:27:02 +0100

    adição de 'estados' para o controle da execução da aplicação em React

--- src/App.js
+++ src/App.js
@@ -47,8 +47,19 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      // irá guardar a lista de Fotografias vindas da API
-      fotos: []
+      /**
+       * irá guardar a lista de Fotografias vindas da API
+       */
+      fotos: [],
+      /**
+       * estados do projeto, durante a leitura de dados na API
+       * @type {"carregando dados" | "erro" | "sucesso"}
+       */
+      loadState: "carregando dados",
+      /**
+       * se algo correr mal, irá aqui ser colocado a mensagem de erro
+       */
+      errorMessage: null,
     }
   }
 
@@ -71,12 +82,21 @@ class App extends React.Component {
      */
     try {
       // 1.
+      this.setState({
+        loadState: "carregando dados"
+      });
       let fotosDaAPI = await getFotos();
+
       // 2.
       this.setState({
-        fotos: fotosDaAPI
+        fotos: fotosDaAPI,
+        loadState: "sucesso"
       });
     } catch (erro) {
+      this.setState({
+        loadState: "erro",
+        errorMessage: erro.toString()
+      });
       console.error("Erro ao carregar os dados das Fotos: ", erro)
     }
   }
@@ -86,22 +106,29 @@ class App extends React.Component {
     // ler os dados existentes no array
     const { fotos } = this.state;
 
-    // este método É OBRIGATÓRIO
-    return (
-      <div className="container">
-        <h1>Fotografias dos Cães</h1>
-        <div className="row">
-          <div className="col-md-8">
-            <h4>Tabela com as fotografias</h4>
-            {/* Tabela5 tem um 'parâmetro de entrada', chamado 'inDadosFotos'.
+    switch (this.state.loadState) {
+      case "carregando dados":
+        return <p>A carregar dados. Aguarde, por favor...</p>
+      case "erro":
+        return <p>Ocorreu um erro: {this.state.errorMessage}.</p>
+      case "sucesso":
+        return (
+          <div className="container">
+            <h1>Fotografias dos Cães</h1>
+            <div className="row">
+              <div className="col-md-8">
+                <h4>Tabela com as fotografias</h4>
+                {/* Tabela5 tem um 'parâmetro de entrada', chamado 'inDadosFotos'.
                 Neste caso, está a receber o array JSON com os dados das fotos dos Cães,
                 lidos da API */}
-            <Tabela inDadosFotos={fotos} />
+                <Tabela inDadosFotos={fotos} />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    );
-
+        );
+      default:
+        return null;
+    }
   }
 }
 
